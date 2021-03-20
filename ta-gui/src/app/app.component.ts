4a5c72fe0bce569b1d8d0a6f50598b6424bdfde0
oxemaxn <maxn@cin.ufpe.br>
2021-03-20 20:49:42 -0300

    Bachup 1

--- ta-gui/src/app/app.component.ts
+++ ta-gui/src/app/app.component.ts
@@ -7,6 +7,7 @@ import { NgModule } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  title = 'ta-gui';
    aluno: Aluno = {nome: "", cpf: "", email: ""};
 }
 
