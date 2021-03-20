3101fdd481ba1ad76214bc874c54a613e2d866b8
oxemaxn <maxn@cin.ufpe.br>
2021-03-20 20:29:36 -0300

    Faltava um conflito ainda

--- ta-gui/src/app/app.module.ts
+++ ta-gui/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 @NgModule({
   declarations: [
     AppComponent
